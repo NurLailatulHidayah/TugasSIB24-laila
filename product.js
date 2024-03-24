b6e07f55554ed847a55e311026c295200d80938c
Nur Lailatul Hidayah <nurlailatulhidayah45@gmail.com>
2024-03-24 15:58:04 +0700

    login dan product responsive

--- product.js
+++ product.js
@@ -1,21 +1,22 @@
 // Mendefinisikan URL API endpoint
-const apiUrl = 'https://crudcrud.com/api/26078e07d5dc4e3d8e04dd36aa2fc78f/products';
+const apiUrl =
+  "https://crudcrud.com/api/26078e07d5dc4e3d8e04dd36aa2fc78f/products";
 
 // Mendapatkan data produk dari API
 fetch(apiUrl)
-  .then(response => {
+  .then((response) => {
     if (!response.ok) {
-      throw new Error('Network response was not ok');
+      throw new Error("Network response was not ok");
     }
     return response.json();
   })
-  .then(data => {
+  .then((data) => {
     // Memproses data produk yang diperoleh dari API
     console.log(data); // Tampilkan data JSON dari respon API
     displayProducts(data); // Panggil fungsi untuk menampilkan produk
   })
-  .catch(error => {
-    console.error('There was a problem with the fetch operation:', error);
+  .catch((error) => {
+    console.error("There was a problem with the fetch operation:", error);
   });
 
 // Fungsi untuk menampilkan produk dalam bentuk daftar
@@ -24,7 +25,7 @@ function displayProducts(products) {
   // productList.innerHTML = ''; // Mengosongkan daftar produk sebelum menambahkan yang baru
 
   // Loop melalui setiap produk dan membuat item daftar
-  products.forEach(product => {
+  products.forEach((product) => {
     const listItem = document.createElement("li");
     listItem.textContent = `${product.name} - Rp ${product.price}`;
     productList.appendChild(listItem);
